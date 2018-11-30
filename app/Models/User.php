83a63f157ed5f0c7e6cfeb21c007d542a05ba9b0
yueruitao <1316751406@qq.com>
2018-11-30 08:14:33 +0000

    add password resets & email configuration

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Notifications\ResetPassword;
 
 class User extends Authenticatable
 {
@@ -27,10 +28,23 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
     
+    public static function boot()
+    {
+        parent::boot();
+        //用于监听模型被创建之前的事件
+        static::creating(function ($user) {
+            $user->activation_token = str_random(30);
+        });
+    }
+    
     public function gravatar($size = '100')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
-
+    
+     public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPassword($token));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -29,9 +29,14 @@ Route::resource('users', 'UsersController');
 //Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
 //Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
 //Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
-
+//登录退出
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
-
-//Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+//发送邮件
+Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+//重置密码
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
